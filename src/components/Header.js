e8bf163698793cc16d168e382af99b4a0ce8f7f3
sumardey5 <sumardey5@gmail.com>
2024-09-19 12:20:35 -0500

    search v1

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,10 @@ const Header = () => {
     return (
         <header className="dashboard-header">
             <div className="header-logo">Brocli</div>  {/* Logo to appear in mobile */}
+            <div className="search-container">
+                <input className="search-input" type="text" placeholder="Search..." />
+                <button className="search-button">🔍</button>
+            </div>
         </header>
     );
 };
